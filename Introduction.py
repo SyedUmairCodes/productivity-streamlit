990628f027188d9b5969ec688138d595b9c87500
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2024-06-23 13:17:06 +0500

    Project completed

---
+++ Introduction.py
@@ -0,0 +1,150 @@
+import streamlit as st
+
+st.set_page_config(page_title=f"A Simple Productivity Guide", page_icon=":100:")
+
+st.write("# :books: How to be More Productive")
+
+st.info(":bulb: These notes are taken from the productivity course from Ali Abdaal and are just my personal notes that I have "
+        "written down while taking the course.")
+
+# Soft and Hard Skills
+st.write("# Hard and Soft Skills")
+st.write("A professional that has been working in his field for over a year or so will pick up a lot of knowledge and"
+         " experience and when he has to solve a problem he will focus on his knowledge and experience that he has "
+         "gained over the years before reaching out to others or look for an external solution.")
+
+st.write("But, this knowledge and experience can only help him with his technical problems that occur and not with a"
+         "problem that has occurred between him and another colleague or manager."
+         "To solve these kinds of problems he will need to use his soft skills that consist of having the ability to "
+         "communicate well with others and handling pressure and managing and even leading teams.")
+
+st.markdown("> Hard skills help you solve technical problems and soft skills help you solve people problems.")
+
+# Productivity
+st.write("# What is Productivity?")
+st.write("## Defining Productivity ")
+st.write("When the industrial revolution started productivity was the amount of product a factory worker or machine "
+         "could make in a day,")
+st.latex( "Productivity = Total\;product / Time\;taken")
+st.write("In our time,  productivity is measured by how many items can you check off of your todo list or how many "
+         "emails can you respond to in a working day.")
+st.write("## The Productivity Equation")
+st.write("Modern productivity can be explained by a simple equation, this equation counts productivity as work that is "
+         "actually useful to the individual or company and not the work we do to get out of our "
+         "actual responsibilities.")
+
+st.markdown("The more `useful output` we can generate in the least amount of time is the amount of `productive work` "
+            "that we did in a day.")
+st.latex(r''' Productivity =\frac{Useful\;output}{Time\;taken} ''')
+st.write("## The Fun Factor")
+st.write("If we're working on a task that is boring and we aren't having fun doing it then our productivity will"
+         "decrease even if we are producing a useful output.  If we're doing something that excites us or is fun to do"
+         "then our productivity increases even if we're doing something that's totally useless. ")
+st.write("Taking this same approach to having fun while doing work we can add a fun factor to almost any task that we "
+         "need to accomplish and it won't be a bother for us since doing that task will be now fun.")
+
+st.latex(r'''Productivity = \frac{Useful\;output}{Time\;taken} \times Fun factor''')
+st.write("By multiplying the amount of useful work done in a day by how fun it was doing it we can see a potentially "
+         "high increase in the productivity of an individual or team.")
+
+st.write("## Fancy Tools")
+st.write("You can use the most fanciest tools available and even use the most number of apps and services that say that"
+         "they increase your productivity but if you don't know how to use the app or the app seems to complex then "
+         "you'll just waste time tinkering with the app or service instead of using it to get actual work done.")
+st.write("> Tools and services don't matter, it's the techniques and systems that matter. ")
+
+# Planning Ahead
+st.write("# Planning Ahead of time")
+st.write("Planning your day the night before is a great approach when you just want to wake up, have breakfast and "
+         "then just start working on the things that matter the most instead of wasting time deciding what task to "
+         "tackle first.")
+st.write("## The Pilot, Plane ,and Engineer")
+st.write("The pilot, plane, and engineer mental model is a mindset that guides you to plan your day ahead and then" 
+         "makes sure that there are no road blocks in your way when your going throughout the day.")
+st.markdown("""
+
+- The pilot is the role you take on every night when you're trying to schedule your tasks for the next
+             day.
+- The plane is the role you take on during the after waking up and already knowing what tasks are needed 
+            to be done first and getting to work.
+- The engineer is a role that is used in conjunction with the plane. Engineer is the role you take on 
+            when there are road blocks in your plane's journey.""")
+
+# Productivity Myths
+st.write("# Myths About Productivity")
+st.write("There are many different myths that people have come up with around productivity and most of them are false "
+         "and most just an excuse to not work.")
+st.write("## Not Enough Time")
+st.write("The most common excuse for not doing work that a person needed to do is that they didn't have enough time to "
+         "do it. Which is totally wrong in a sense that all human beings get 24 hours in a day and how you choose to "
+         "spend those 24 hours is up to you.")
+st.write("You can finish your required tasks and then go to the gym or take a walk outside, or even cook a nice dinner "
+         "but most people would rather prefer takeout, watching Netflix, and scrolling through social media.")
+st.write("> You already have the time to do everything you want to do. It's just that you choose not to do it.")
+
+st.write("## Not Enough Motivation")
+st.write("The second myth is also kind of made up, people don't work towards there goals that they set because they "
+         "don't see an immediate result and feel like they haven't made any progress at all and give up half way,")
+st.write("Motivation isn't the thing that you should use an excuse because motivation is just a little burst of energy "
+         "that everyone gets when we are relaxed and excited for something. Once that burst of energy dies down then "
+         "we're back to our old selves again.")
+st.write("Instead of using Motivation as a fuel source for achieving your goals make it a habit of doing the same task"
+         "every day and be consistent with completing it.")
+st.write("Being consistent is what will give you the results you want the fastest and will make you feel more motivated"
+         "towards completing your goals and the more goals you complete the more motivated you'll become.")
+
+st.write("## Always Multitasking")
+st.write("Human brains are not made for doing multiple things at once and multitasking may seem like it's a great way "
+         "to increase our productivity but in reality it's not.")
+st.write("If you focus on a single task at a time and give all you attention to that task then you'll enter your flow "
+         "state. The flow state is the state in which your body and mind are at their peek performance levels and your "
+         "productivity is also at it's peak.")
+st.write("Multitasking doesn't let this happen because whenever you switch tasks you also shift your mental context "
+         "and the brain needs time to adapt to this new context which also decreases your productivity.")
+
+# Laws of Productivity
+st.write("# Scientific Laws and Productivity")
+st.write("There are many different scientific laws that we can use in our daily lives in order to increase our "
+         "productivity. ")
+st.write("## Parkinson's Law")
+st.write("This law states that work will expand over time that we have allocated it. Meaning that the more time you "
+         "give to complete a task the more time it will take to complete it.")
+st.write("Parkinson's law helps us to boost our productivity by assigning ourselves `artificial` deadlines."
+         "Complete an assignment today whose deadline is next week, by doing this we are generating more useful output"
+         "in the least amount of time and are not letting our work expand over time.")
+st.write("## Pareto's Principle")
+st.write("Pareto's principle suggests that 80% of our work can be completed in just 20% of the effort. ")
+st.write("The Pareto principle blends into productivity easily because if you are studying for an upcoming test it's a "
+         "proven fact that 80% of the test questions will come from the 20% of the syllabus which is the core part of "
+         "that subject.")
+st.write("## Newton's First Law of Motion")
+st.write("Newton's first law of motion is also applicable to productivity and how we can increase it.")
+st.write("> An object will stay at rest or travel at a constant velocity until a external forced is applied on it.")
+st.write("This means if we are procrastinating we will continue to procrastinate until our deadline comes close or "
+         "someone tells us to get back to work. This can also be used in the opposite way as well.")
+st.write("## Two and Five Mintue Rules")
+st.write("The two minute rule states that if a task can be completed in less than two minutes then it should be done "
+         "immediately and if left undone this two minute tasks could pile up and become  2+ hours of work.")
+st.write("The five minute rule states that if there is any task that needs to be completed but we aren't in a mood to"
+         "complete it then just try working on it for five minutes as this tricks the brain into concentrating on the "
+         "task and eventually completing it,")
+
+# Making productivity fun
+st.write("# Making Productivity Fun")
+st.write("## Healthy Habits")
+st.write("One of the best ways to be productive is to make a daily task such as handling emails into a habit."
+         "Pick a time in your day and manage your emails daily at that time so that you don't have a huge inbox filled "
+         "with thousands of emails that need to be responded to.")
+st.write("> Building good habits is not a temporary goal but to build a better routine and overall self-image.")
+st.write("## Productive Downtime")
+st.write("You'll see that most people will have all the time in the world and will automatically be doing more than it "
+         "looks like. This is because they make their down time into productive time as well.")
+st.write("Down time is the break you take between tasks, if you have sent an email to someone and need their response "
+         "before continuing your work read a book or article that interests you instead of scrolling social media or"
+         " playing games on your phone while you wait.")
+st.write("## Productive Procrastination")
+st.write("If you have a deadline to meet at the end of the month then the best thing would be get started right now as"
+         "you could complete your task before the deadline and create more useful output.")
+st.write("But, like most people you'll probably procrastinate and while procrastinating isn't ideal it doesn't have to "
+         "be bad. You can learn more about your task by watching videos on YouTube about it instead of cat videos.")
+st.write("If you can make your procrastination period into productive time then you'll be more productive than ever.")
